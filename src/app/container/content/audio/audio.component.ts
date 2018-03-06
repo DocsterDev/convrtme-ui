4c5b4ffd0564b50c30305a058e1926e8817b6616
Unknown <jeffreysapia@gmail.com>
2018-03-05 19:38:33 -0800

    Adding increments

--- src/app/container/content/audio/audio.component.ts
+++ src/app/container/content/audio/audio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-audio',
@@ -7,13 +7,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudioComponent implements OnInit {
 
-  constructor() { }
+  private map = new Map();
 
-  ngOnInit() {
+  public audioData = [
+    {
+      uuid: 'abc1234',
+      title: 'Gagnum Style [Official Video]',
+      conversionFrom: 'MP3',
+      conversionTo: 'WMA',
+      incrementValue: 100
+    },
+    {
+      uuid: 'abc1675',
+      title: 'Green Day - When I Come Around',
+      conversionFrom: 'WAV',
+      conversionTo: 'OGG',
+      incrementValue: 88
+    },
+    {
+      uuid: 'abc6743',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 100
+    },
+    {
+      uuid: 'abc9877',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 74
+    },
+    {
+      uuid: 'abc4845',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 100
+    },
+    {
+      uuid: 'abc4568',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 100
+
+    },
+    {
+      uuid: 'abc7547',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 95
+    },
+    {
+      uuid: 'abc3467',
+      title: 'Good Charlotte - The Anthem',
+      conversionFrom: 'MP3',
+      conversionTo: 'FLAC',
+      incrementValue: 100
+    }
+  ];
+
+  constructor() {
   }
 
-  public onUpload(){
+  ngOnInit() {
+    /**
+     * Load map will simulate when audio data is loaded after every server load and update of list
+     */
+    this.loadMap();
+  }
+
+  public onUpload() {
     console.log('Upload files');
+  }
+
+  /**
+   * Add 1 to the progress bar
+   */
+  public increment(uuid) {
+    const val = this.map.get(uuid);
+    if (val.incrementValue === 100) {
+      return;
+    }
+    val.incrementValue++;
+  }
+
+  /**
+   * Load the map with key of uuid
+   */
+  public loadMap() {
+    this.audioData.forEach((e) => {
+      this.map.set(e.uuid, e);
+    });
   }
 
 }
