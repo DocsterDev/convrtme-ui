0e77cadf36d6e4f58ae7a74c3edd5c1ea37d9b61
Unknown <jeffreysapia@gmail.com>
2018-06-09 20:06:41 -0700

    update

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
+  //private host = 'jeff-staging.mynetgear.com';
   private host = '192.168.0.15';
   private port = '8080';
 
