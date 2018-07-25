6be25aae0eb04bbe3026dd6c0472d64f436b4446
Unknown <jeffreysapia@gmail.com>
2018-07-24 20:45:21 -0700

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from '@angular/router';
 import {contentRoutes} from './container/content/content.routes';
 import {ContentComponent} from './container/content/content.component';
+import {YoutubeComponent} from './container/content/youtube/youtube.component';
 
 export const appRoutes: Routes = [
-  {path: 'app', component: ContentComponent},
-  {path: 'app', children: contentRoutes}
+  {path: '', component: YoutubeComponent},
+  //{path: 'app', children: contentRoutes}
 ];
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  //private host = 'jeff-staging.mynetgear.com';
-  private host = 'localhost';
+  private host = 'ec2-54-153-119-154.us-west-1.compute.amazonaws.com';
   private port = '8083';
 
 
