5931967c21964057cb7f40b99fd7e0f3dd7f7750
Unknown <jeffreysapia@gmail.com>
2018-07-24 20:46:14 -0700

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import {Routes} from '@angular/router';
-import {contentRoutes} from './container/content/content.routes';
-import {ContentComponent} from './container/content/content.component';
 import {YoutubeComponent} from './container/content/youtube/youtube.component';
 
 export const appRoutes: Routes = [
-  {path: '', component: YoutubeComponent},
-  //{path: 'app', children: contentRoutes}
+  {path: '', component: YoutubeComponent}
 ];
