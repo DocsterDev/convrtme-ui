e89c5c9f8b379efbf20a84f5efd31ec1a064edfd
Unknown <jeffreysapia@gmail.com>
2018-07-27 20:58:15 -0700

    Update ui

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -3,11 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  //private host = 'ec2-52-53-165-205.us-west-1.compute.amazonaws.com';
-  // private host = 'ec2-54-153-119-154.us-west-1.compute.amazonaws.com';
-  // private port = '8083';
+  // TODO A Build for prod: ng build --aot --prod --base-href=./
 
-  private host = 'localhost';
+  private host = 'ec2-52-53-165-205.us-west-1.compute.amazonaws.com';
+  // private host = 'localhost';
   private port = '8083';
   private streamPort = '8084'
 
