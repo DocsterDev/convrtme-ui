16dec6fa4a88bda6872f827bf9379db7a2d6c0a3
Unknown <jeffreysapia@gmail.com>
2018-08-14 18:01:23 -0700

    Update UI

--- src/app/service/header.service.ts
+++ src/app/service/header.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {LocalStorageService} from "ngx-webstorage";
-import {RequestOptions} from "@angular/http";
 
 @Injectable()
 export class HeaderService {
@@ -12,7 +11,7 @@ export class HeaderService {
 
   public getTokenHeader() {
     const token = this.localStorage.retrieve('token');
-    console.log('TOKEN: ' + token);
+
     if (token) {
       return {
         headers: new HttpHeaders({
