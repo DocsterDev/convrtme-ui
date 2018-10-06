525a4d417cbf964a8bf40324754099c4c55ccf2d
Unknown <jeffreysapia@gmail.com>
2018-10-06 14:58:31 -0700

    updree

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-52-53-165-205.us-west-1.compute.amazonaws.com:8083',
-  streamUrl: 'http://ec2-52-53-165-205.us-west-1.compute.amazonaws.com:8084'
+  apiUrl: 'http://ec2-54-183-242-32.us-west-1.compute.amazonaws.com:8083',
+  streamUrl: 'http://ec2-54-183-242-32.us-west-1.compute.amazonaws.com:8084'
 };
