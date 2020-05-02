dea6240d409b75fa04fc2f62630fdbca139f42ed
Jeff <jeffreysapia@gmail.com>
2020-05-02 17:56:13 -0500

    MOUP-RESURRECTION-PROJECT: Adding new API endpoint.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-34-207-194-1.compute-1.amazonaws.com:8083',
-  streamUrl: 'http://ec2-34-207-194-1.compute-1.amazonaws.com:8084'
+  apiUrl: 'http://api.moup.io:8083',
+  streamUrl: 'http://api.moup.io:8084'
 };
